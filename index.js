51d37d5cdb6e3623c8a9a5ef69469050229cb916
LordNinth <rr621801@gmail.com>
2024-02-27 22:15:42 +0000

    protecting content using predefined middleware

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
+const { Experimental_CssVarsProvider } = require("@mui/material");
 const express = require("express");
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log("first middleware");
+//   return next(); //stops functions execution, i.e following line wont run
+//   console.log("first middleware-after calling next");
+// });
+
+// app.use((req, res, next) => {
+//   console.log("second middleware");
+//   next();
+// });
+
+// defining our first middleware, it runs first
+
+const verifyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === "chickennugget") {
+    next(); //if we dont add this, next middleware wont run
+  }else{
+    res.send('sorry you need a password')
+  }
+};
 app.use((req, res, next) => {
-  console.log("first middleware");
+  req.requestTime = Date.now(); //adding method to req. So I have access to request time on every route handler
+  console.log(req.method, req.path);
   next();
-  console.log("first middleware-after calling next");
 });
 
-app.use((req, res, next) => {
-  console.log("second middleware");
-  next();
-});
 
+//after verifyPassword, whats next is the (req,res) callback. so it will run. 
+//Next() depends on where the middleware is run. 
+app.get('/secret',verifyPassword, (req,res)=>{ 
+    res.send("sometimes i dress like a carrot and sit on the yard")
+})
 app.get("/", (req, res) => {
-  res.send("hello from home");
+  console.log(`request date: ${req.requestTime}`);
+  res.send("hello from home"); //after this, whole req/res cycle stops
 });
+
+app.use((req,res)=>{
+    res.status(404).send("not found")
+})
 app.listen("3000", (req, res) => {
   console.log("listening to 3000");
 });
--- note.md
+++ note.md
@@ -1,5 +1,19 @@
 Middleware
+It has access to req and res objects
 
 If we are to define custom error handling middleware: we need (err,req,res,next)
 
 and we must specify: next(err) to move it to second error handle. If we do not include err, it will go to normal middle ware
+
+--- 
+npm i morgan
+ const morgan = require('morgan')
+
+app.use(morgan('common'))
+it is a logger
+
+---
+app.use
+runs for every incoming req
+we can use it define 404 route:
+    put app.use at the end of file, so if res/req cycle doesn't end, app.use will send 404 not found
