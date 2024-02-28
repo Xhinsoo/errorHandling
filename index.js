d205283ce0bafbb1825dcf9443f4ac69b5278683
LordNinth <rr621801@gmail.com>
2024-02-28 19:20:19 +0000

    error handling and middleware

--- index.js
+++ index.js
@@ -43,18 +43,32 @@ app.get("/", (req, res) => {
 app.get("/chicken", (req, res) => {
   chicken.fly();
 });
+app.get('/admin', (req,res)=>{
+  throw new AppError("you are not admin", 401)
+})
+
 app.use((req, res) => {
   res.status(404).send("not found");
 });
 
 //logging error and passing to default error handling
+// app.use((err, req, res, next) => {
+//   console.log("*******error*******");
+//   console.log("*****************");
+//   // res.status(500).send("oh we have error")
+//   next(err); //if i dont write next, req/res cycle ends,
+//   // we need to pass err in next() to let default error handling function
+// });
+
+//error handling
 app.use((err, req, res, next) => {
-  console.log("*******error*******");
-  console.log("*****************");
-  // res.status(500).send("oh we have error")
-  next(err); //if i dont write next, req/res cycle ends,
-  // we need to pass err in next() to let default error handling function
+  const {status,message} = err
+  res.status(status).send(message)
+
+
 });
+
+
 app.listen("3000", (req, res) => {
   console.log("listening to 3000");
 });
